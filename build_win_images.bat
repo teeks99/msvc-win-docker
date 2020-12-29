54d4fa3750a79f3e9e33472a801e9737722a46d0
Thomas Kent <tom@teeks99.com>
2020-12-29 09:25:24 -0600

    More tags

--- build_win_images.bat
+++ build_win_images.bat
@@ -1,6 +1,11 @@
 
+set datestr=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
+
 for %%v in ( 14.1 14.2 ) do (
   pushd msvc-%%v
-  docker build -t teeks99/msvc-win:%%v -m 4g .
+  docker build -t teeks99/msvc-win:%%v_%datestr% -m 4g .
+  docker tag teeks99/msvc-win:%%v_%datestr% teeks99/msvc-win:%%v
   popd
 )
+
+docker tag teeks99/msvc-win:14.2 teeks99/msvc-win:latest
--- push_win_images.bat
+++ push_win_images.bat
@@ -1,3 +1,8 @@
+set datestr=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
+
 for %%v in ( 14.1 14.2 ) do (
+  docker push teeks99/msvc-win:%%v_%datestr%
   docker push teeks99/msvc-win:%%v
 )
+
+docker push teeks99/msvc-win:latest
